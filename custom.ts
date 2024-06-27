0de259517eb44767f795685915c6d17f46cb4138
neonerz <gerald@rewrite.media>
2024-06-27 09:40:37 -0400

    Update custom.ts

--- custom.ts
+++ custom.ts
@@ -9,7 +9,7 @@ enum Direction {
 namespace custom {
 
     //% block="agent move %move_direction %move_blocks blocks then replace %replace_direction with %block"
-    export function agent_move_replace(move_direction: Direction, replace_direction: Direction, move_blocks: number, block: Block): void {
+    export function agent_move_replace(move_direction: Direction, move_blocks: number, replace_direction: Direction, block: Block): void {
         agent.move(move_direction, move_blocks)
         agent.destroy(replace_direction)
         agent.setItem(block, 1, 1)
